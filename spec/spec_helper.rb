64f58f973445111f3bc48e529bd0b398611c4447
Peter Yates <peter.yates@graphia.co.uk>
2022-10-01 20:18:20 +0100

    Move common testing helpers to a shared location
    We'll soon introduce a new spec file and we want to share this
    functionality.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,9 @@
 require_relative "./../lib/govuk_markdown"
+
+def render(content)
+  GovukMarkdown.render(content)
+end
+
+def expect_equal_ignoring_ws(first, second)
+  expect(first.lines.map(&:strip).join("")).to eq(second.lines.map(&:strip).join(""))
+end
