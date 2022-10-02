8a20e10bd9bd94cfee5c27c7bf2e3ef9b2c08823
Peter Yates <peter.yates@graphia.co.uk>
2022-10-02 16:15:05 +0100

    Make the regexp building reusable

--- lib/govuk_markdown/preprocessor.rb
+++ lib/govuk_markdown/preprocessor.rb
@@ -7,13 +7,23 @@ module GovukMarkdown
     end
 
     def inject_inset_text
-      document.gsub(%r(\{inset-text\}(.*)\{/inset-text\})m) do
+      document.gsub(build_regexp("inset-text")) do
         <<~HTML
           <div class="govuk-inset-text">
             #{Regexp.last_match(1)}
           </div>
         HTML
       end
+    end
+
+  private
+
+    def build_regexp(tag_name, pre_tag: "{", post_tag: "}", closing: "/")
+      before  = pre_tag +           tag_name + post_tag
+      after   = pre_tag + closing + tag_name + post_tag
+      pattern = [Regexp.quote(before), "(.*)", Regexp.quote(after)].join
+
+      Regexp.compile(pattern, Regexp::EXTENDED | Regexp::MULTILINE)
     end
   end
 end
